4e0984ef75445fd9e95f3fa153e0531a1a0f8390
Ziyan Mo <moziyan@yahoo.com>
2019-07-14 19:38:34 -0700

    Complete user model

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,9 +1,157 @@
-import {Schema, model} from 'mongoose';
+import {Schema, model, Model, Document} from 'mongoose';
 
 export interface User {
-	// TODO: add properties
+	// TODO: add properties 
 }
 
-const schema = new Schema({});
+export interface Application extends User, Document{
+  phoneNumber: string;
+  school: string;
+  major: string;
+  undergrad: string;
+  graduationYear: string;
+  github?: string;
+  linkedin?: string;
+  resume: string;
+  latino: boolean;
+  race: string;
+  gender: string;
+  shirtSize: string;
+  needTransportation: boolean;
+  busFrom?: string;
+  airport?: string;
+  dietaryRestrictions: string;
+  dietaryRestrictionsDetail?: string;
+  Q1: string;
+  Q2: string;
+  Q3: string;
+  Q4: string;
+  applicationStatus: string;
+  decidedBy: string;
+  reimbursementGiven?: boolean;
+}
 
-export const User = model('User', schema);
+let application = {
+  phoneNumber: String,
+  school: {
+    type: String,
+    min: 1,
+    max: 150,
+  },
+
+  major: String,
+
+  undergrad: {
+    type: String,
+    enum : {
+      values: "High School, Undergraduate, Graduate, Postdoc"
+    }
+  },
+
+  graduationYear: {
+    type: String,
+    enum: {
+      values: '2020 2021 2022 2023 2024+'.split(' ')
+    }
+  },
+
+  github: String,
+  linkedin: String,
+  resume: String,
+
+  latino: Boolean,
+  race:{
+    type: String,
+      enum: {
+        values: `Asian,
+                 White,
+                 Native Hawaiian or Other Pacific Islander,
+                 American Indian or Alaska Native,
+                 Black or African American`.split(', ')
+      }
+  },
+
+  gender: {
+    type: String,
+      enum: {
+        // Male, Female, Other, prefer not to answer
+        values: "M F O P".split(' ')
+      }
+  },
+
+  shirtSize: {
+    type: String,
+      enum: {
+        values: "Small Medium Large X-Large".split(' ')
+      }
+  },
+
+  needTransportation: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+
+  busFrom: {
+    type: String,
+      enum: {
+        // TODO: Add others!
+        values: "UCLA, UC Berkeley, Stanford, UCSD, USC".split(', ')
+      }
+  },
+
+  airport: {
+    type: String,
+  },
+
+  dietaryRestrictions: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+
+  dietaryRestrictionsDetail: {
+    type: String,
+  },
+
+  Q1: {
+    type: String,
+    max: 1500,
+  },
+
+  Q2: {
+    type: String,
+    max: 1500,
+  },
+
+  Q3: {
+    type: String,
+    max: 1500,
+  },
+
+  Q4: {
+    type: String,
+    max: 1500,
+  },
+
+  applicationStatus: {
+    type: String,
+    enum: {
+      values: "In-progress Submitted Accepted Denied Declined".split(' ')
+    }
+  },
+
+  decidedBy: {
+    type: String,
+  },
+
+  reimbursementGiven: {
+    type: Boolean,
+  }
+}
+
+const schema = new Schema({
+  application : application,
+});
+
+export const User: Model<Application> = model<Application>("User", schema);
