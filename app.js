7dbd4a59d359f1b9f4252a1f33b864eb6b609f98
Ziyan Mo <moziyan@yahoo.com>
2019-07-14 18:56:41 -0700

    Add user model

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express	= require('express');
-var port 	= 3000;
-var app		= express();
+var express 	= require('express');
+var port 	   = 3000;
+var app		    = express();
+const assert  = require('assert');
+var mongoose  = require('mongoose');
+const url = 'mongodb://localhost:27017/hacktech2020';
 
 app.use(express.static(__dirname + '/client'));
-const assert = require('assert');
-var mongoose        = require('mongoose');
-
-const url = 'mongodb://localhost:27017/hacktech2020';
 mongoose.connect(url, { useNewUrlParser: true });
-app.listen(port, () => console.log(`Example app listening on port ${port}, directory`+__dirname));
+app.listen(port, () =>
+console.log(`Example app listening on port ${port}, directory`+__dirname));
