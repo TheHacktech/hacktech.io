f4836406537f16d22968430eb319873261f0f0d9
Ziyan <moziyan@Yahoo.com>
2019-08-19 04:04:24 +0000

    Add application skeleton and routing skeleton

---
+++ client/index_content.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index_content.html',
+  styleUrls: [ './static/assets/css/index.css' ]
+})
+export class IndexComponent  {}
--- client/routes.ts
+++ client/routes.ts
@@ -2,24 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 //Add your components here
+import { AppComponent } from
+  './app.component';
 import { ApplicationComponent } from 
   './views/application/application.component';
-import { AppComponent } from 
-  './index';
+import { IndexComponent } from
+  './index_content';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule, 
+    FormsModule,
     ReactiveFormsModule,
-    RouterModule.forChild([
+    RouterModule.forRoot([
       { path: 'application', component: ApplicationComponent },
+      { path: '',  component: IndexComponent},
     ])
   ],
   declarations: [
+    AppComponent,
     ApplicationComponent, 
-    AppComponent
+    IndexComponent, 
   ],
   bootstrap: [ AppComponent ]
 })
